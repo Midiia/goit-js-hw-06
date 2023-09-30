2c4c0e1af79628d258c06e6112771ebf03ac02f0
Midiia <midiya1993@gmail.com>
2023-09-30 23:42:22 +0200

    hw update

--- js/task-02.js
+++ js/task-02.js
@@ -9,18 +9,26 @@ const ingredients = [
 
 const list = document.querySelector(`#ingredients`);
 
-const firstItem = document.createElement("li");
-firstItem.textContent = "Potatoes";
-const secondItem = document.createElement("li");
-secondItem.textContent = "Mushrooms";
-const thirdItem = document.createElement("li");
-thirdItem.textContent = "Garlic";
-const fourthItem = document.createElement("li");
-fourthItem.textContent = "Tomatos";
-const fifthItem = document.createElement("li");
-fifthItem.textContent = "Herbs";
-const sixItem = document.createElement("li");
-sixItem.textContent = "Condiments";
+ingredients.map((ingr) => {
+  const item = document.createElement("li");
+  item.textContent = ingr
+  return list.append(item);
+});
 
 
-list.append(firstItem, secondItem, thirdItem, fourthItem, fifthItem, sixItem);
+
+// const firstItem = document.createElement("li");
+// firstItem.textContent = "Potatoes";
+// const secondItem = document.createElement("li");
+// secondItem.textContent = "Mushrooms";
+// const thirdItem = document.createElement("li");
+// thirdItem.textContent = "Garlic";
+// const fourthItem = document.createElement("li");
+// fourthItem.textContent = "Tomatos";
+// const fifthItem = document.createElement("li");
+// fifthItem.textContent = "Herbs";
+// const sixItem = document.createElement("li");
+// sixItem.textContent = "Condiments";
+
+
+// list.append(firstItem, secondItem, thirdItem, fourthItem, fifthItem, sixItem);
